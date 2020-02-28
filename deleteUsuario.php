87bc6e978bfdb4e8b55e52181708a1a49192eb9a
deborabastos <deborabastos14@gmail.com>
2020-02-28 20:16:52 -0300

    deleteProduto

--- deleteUsuario.php
+++ deleteUsuario.php
@@ -1,19 +1,21 @@
 <?php
+session_start();
+
 // ********************************* CHAMA FUNÇÕES *********************************
-require_once('usuarios.php');
+require('./includes/produtos.inc.php');
 
 
-// Verifica ID, se não, retorna erro
+// Verifica se foi passado parâmetro ID, se não, retorna erro
 if (!isset($_POST['id'])){
     include_once('./includes/not_found.php');
     exit;
 }
 
-//Pelo ID procura o usuario com essa ID no json, retornando dados na variável $usuario
-$usuarioId = $_POST['id'];
-deleteUsuario($usuarioId);
+//Pelo ID procura o produto com essa ID no json, retornando dados na variável $produto
+$produtoId = $_POST['id'];
+deleteProduto($produtoId);
 
-header('location: createUsuario.php');
+header('location: indexProduto.php');
 
 
 ?>
